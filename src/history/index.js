798f3c90403abf50b0552d3e78014f22d24b12e7
Brendon Daugherty <brendon1097@gmail.com>
2018-05-06 11:39:10 -0700

    Woops

--- src/history/index.js
+++ src/history/index.js
@@ -1,5 +1,5 @@
 import createHistory from 'history/createBrowserHistory'
 
 // Create a browser histroy
-const history = createHistory()
+const history = createHistory({basename:process.env.PUBLIC_URL})
 export default history
