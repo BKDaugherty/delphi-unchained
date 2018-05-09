fdd3436102bacbc0e44e2b2b1f3759fe8942cb79
Brendon Daugherty <brendon1097@gmail.com>
2018-05-08 21:33:17 -0700

    Added a lil more to Stake View Card and expansion panel logic

--- src/components/ClaimView.js
+++ src/components/ClaimView.js
@@ -2,19 +2,52 @@ import React from 'react'
 import Typography from 'material-ui/Typography'
 import PropTypes from 'prop-types'
 
+import Card, {CardContent} from 'material-ui/Card'
+
+import ExpansionPanel, {
+    ExpansionPanelSummary,
+    ExpansionPanelDetails,
+  } from 'material-ui/ExpansionPanel';
+
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+
+const RulingStatus = ["Justified", "Not Justified", "Collusive", "Ruling Failure"]
+
 
 // Renders a single claim
 const ClaimView = ({claim}) => (
-    <Typography color="textSecondary">
-        Claim ID:  {claim.id} <br />
-        Claim Amount: {claim.amount}<br />
-        Fee: {claim.fee}<br />
-        Surplus Fee: {claim.surplus_fee}<br />
-    </Typography>
+    <Card>
+        <CardContent>
+            <Typography color="textSecondary">
+                Claim ID:  {claim.id} <br />
+                Claim Amount: {claim.amount}<br />
+                Fee: {claim.fee}<br />
+                Data: {claim.data}<br />
+
+                {/* Not what I'd like to do here... */}
+                {claim.ruled ? `Ruling Status: ${RulingStatus[claim.ruling]}` : null} 
+                {claim.ruled ? <br/> : null}                    
+                {claim.settlementFailed? `The settlement failed.` : null}
+            </Typography>
+        </CardContent>
+    </Card>
+)
+
+// Renders the Claim View inside of an expansion panel!
+const ClaimExpansion = ({claim}) => (
+    <ExpansionPanel>
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+            Claim ID: {claim.id}
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+            <ClaimView claim={claim}/>
+        </ExpansionPanelDetails>
+    </ExpansionPanel>
 )
 
 ClaimView.propTypes = {
     claim: PropTypes.object.isRequired,
 }
 
-export default ClaimView
+export default ClaimExpansion
--- src/components/StakeViewCard.js
+++ src/components/StakeViewCard.js
@@ -4,31 +4,42 @@ import Typography from 'material-ui/Typography'
 import Button from 'material-ui/Button'
 import PropTypes from 'prop-types'
 
+import Grid from 'material-ui/Grid'
 import ClaimView from './ClaimView'
 
 // Renders the information on a stake
 const StakeViewCard = (props) => {
     const { classes, stake } = props
     const claims_list = stake.claims
+    const whitelisted_claimants = stake.whitelisted_claimants
 
     return (
         <Card className={classes.card}>
             <CardContent>
-                <Typography className={classes.title} color="textSecondary">
-                    Information for stake address: {stake.token.address}
+                <Typography variant="headline" component="h2"  color="textSecondary">
+                    Stake Address: {stake.token.address}
                 </Typography>
-                <Typography variant="headline" component="h2">
-                </Typography>
-              
                 <Typography className={classes.pos} color="textSecondary">
                     Stake Amount: {stake.value} <br />
                     Claim Deadline: {stake.claim_deadline} <br />
-                    Number of Current Claims: {claims_list.length}
                 </Typography>
-                {/* Conditionally render the claims given, replaces the old formatClaimInfo()*/}
-                {claims_list.length > 0 ? claims_list.map(claim => <ClaimView key={claim.id} claim={claim}/>) : null}
-                
+                <Grid direction='row' container spacing={24}>
+                    <Grid md={6} item spacing={16}>
+                        <Typography variant='headline' component='h3' color="textSecondary">
+                            Claims
+                        </Typography>
+                        {claims_list.length > 0 ? claims_list.map(claim => <Grid item><ClaimView key={claim.id} claim={claim}/></Grid> ) : null}
+                    </Grid>
+                    <Grid md={6} item spacing={16}>
+                        <Typography variant='headline' component='h3' color="textSecondary">
+                            Whitelisted Claimants
+                        </Typography>
+                        {whitelisted_claimants.length > 0 ? whitelisted_claimants.map(addr => <Grid item><Typography key={addr}>{addr}</Typography></Grid>) : null}
+                    </Grid>
+                </Grid>
+
             </CardContent>
+
             <CardActions>
               <Button size="small">Learn More About This Staker</Button>
               {/* {ethAddress === stake.staker && <Button size="small">Whitelist a claimant</Button>}  */}
@@ -36,6 +47,9 @@ const StakeViewCard = (props) => {
         </Card>
     )
 }
+
+
+
 
 StakeViewCard.propTypes = {
     classes: PropTypes.object.isRequired,
