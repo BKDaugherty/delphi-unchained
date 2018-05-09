1598ec8c2e84cae6ccc7738bbd157e5e865901af
Brendon Daugherty <brendon1097@gmail.com>
2018-05-09 15:33:15 -0700

    Networking

--- src/services/delphi-backend/Stake.js
+++ src/services/delphi-backend/Stake.js
@@ -3,7 +3,7 @@ import {API_URL, API_PORT, ENDPOINTS } from './config'
 const GetStakeInfoAtAddress = async (address) => {
     try {
         const request_url = `http://${API_URL}:${API_PORT}${ENDPOINTS.GETSTAKE(address)}`
-        const stakeInfo = await fetch(request_url)
+        const stakeInfo = await fetch(request_url, {mode:'no-cors'})
         const stakeJson = await stakeInfo.json()
         return stakeJson.data
     } catch (error) {
--- src/services/delphi-backend/config.js
+++ src/services/delphi-backend/config.js
@@ -1,6 +1,6 @@
 // Defines the location of the API
-export const API_URL = "localhost"
-export const API_PORT = 5432
+export const API_URL = "0.0.0.0"
+export const API_PORT = 5000
 
 // Defines the endpoints of the API that we can access 
 export const ENDPOINTS = {
