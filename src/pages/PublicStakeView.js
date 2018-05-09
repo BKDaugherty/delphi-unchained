833cb5d7c4166f510bc9177790ac5825856d027b
Brendon Daugherty <brendon1097@gmail.com>
2018-05-09 12:39:08 -0700

    Setup for demo

--- src/pages/PublicStakeView.js
+++ src/pages/PublicStakeView.js
@@ -9,6 +9,7 @@ import React from 'react'
 // Import the pieces of Material-UI we need
 import { withStyles } from 'material-ui/styles'
 import Paper from 'material-ui/Paper'
+import Button from 'material-ui/Button'
 
 // Import our custom components
 import AppHeader from '../components/AppHeader'
@@ -63,71 +64,19 @@ class PublicStakeView extends React.Component {
         this.contracts = context.drizzle.contracts
     }
 
+
+
     state = {
-        stakeInfo: {
-            "staker": "0x627306090abab3a6e1400e9345bc60c78a8bef57",
-            "value": 100,
-            "token": {
-              "name": "DelphiCoin",
-              "symbol": "DC",
-              "address": "0x5ed8cee6b63b1c6afce3ad7c92f4fd7e1b8fad9f"
-            },
-            "minimum_fee": 5,
-            "data": "QmWWQSuPMS6aXCbZKpEjPHPUZN2NjB3YrhJTHsV4X3vb2t",
-            "claim_deadline": 1525417344,
-            "arbiter": {
-              "name": "",
-              "description": "",
-              "address": "0x498bad589c7acd871945ed6ca30b7bab0a977af7"
-            },
-            "whitelisted_claimants": [
-            "0x5ed8cee6b63b1c6afce3ad7c92f4fd7e1b8fad9f",
-            "0x498bad589c7acd871945ed6ca30b7bab0a977af7",
-            "0x554f8e6938004575bd89cbef417aea5c18140d92"
-          ],
-          "claims": [
-            {
-              "id": 0,
-              "amount": 25,
-              "fee": 5,
-              "surplus_fee": 0,
-              "data": "QmT4AeWE9Q9EaoyLJiqaZuYQ8mJeq4ZBncjjFH9dQ9uDVA",
-              "ruling": 0,
-              "ruled": 0,
-              "settlement_failed": 1
-            },
-            {
-              "id": 1,
-              "amount": 25,"fee": 5,
-              "surplus_fee": 0,
-              "data": "QmT4AeWE9Q9EaoyLJiqaZuYQ8mJeq4ZBncjjFH9dQ9uDVA",
-              "ruling": 0,
-              "ruled": 0,
-              "settlement_failed": 1
-            },
-            {
-              "id": 2,
-              "amount": 25,
-              "fee": 5,
-              "surplus_fee": 0,
-              "data": "QmT4AeWE9Q9EaoyLJiqaZuYQ8mJeq4ZBncjjFH9dQ9uDVA",
-              "ruling": 0,
-              "ruled": 0,
-              "settlement_failed": 1
-            },
-          ],
-          "settlements": [
-            {
-              "amount": 10,
-              "staker_agrees": 0,
-              "claimant_agrees": 0
-            }
-          ]
-        }
+        stakeInfo:null
     }
 
-    getData(address){
-        const stakeInfo = GetStakeInfoAtAddress(address)
+    componentDidMount(){
+        // Get data for given stake
+        this.getData(this.props.match.params.address)
+    }
+
+    async getData(address){
+        const stakeInfo = await GetStakeInfoAtAddress(address)
         this.setState({stakeInfo, ...this.state})
     }
 
@@ -141,11 +90,12 @@ class PublicStakeView extends React.Component {
             <div>
                 <AppHeader ethAddress={ethAddress}/>
                 <div className={classes.toolbar}/>
-                <StakeCard className={classes.root} stake={this.state.stakeInfo} classes={classes} address={match.params.address} contract={this.contracts.DelphiStake} ethAddress={ethAddress}/>
-                    {/* {(ethAddress && (ethAddress.toLowerCase()  === this.state.stakeInfo.staker)) && <DrizzledOwnerActions/>} */}
-                {/* <Button raised onClick={() => this.getData(match.params.address)}>
+                <h2>{match.params.address}</h2>
+                {/* Conditionally render the stake*/}
+                {this.state.stakeInfo && <StakeCard className={classes.root} stake={this.state.stakeInfo} classes={classes} address={match.params.address} contract={this.contracts.DelphiStake} ethAddress={ethAddress}/>}
+                {<Button onClick={() => this.getData(match.params.address)}>
                     Refresh Stake
-                </Button> */}
+                </Button>}
             </div>
         )
     }
