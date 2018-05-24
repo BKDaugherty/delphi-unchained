52700170f85c9eac4969f765cc2758651f83ef8a
Brendon Daugherty <brendon1097@gmail.com>
2018-05-24 16:22:42 -0700

    Restructured backend config

--- src/services/delphi-backend/config.js
+++
@@ -1,8 +0,0 @@
-/*
-    Defines constants for communicating with the API service
-*/
-
-// Defines the endpoints of the API that we can access
-export const ENDPOINTS = {
-    GETSTAKE : address => `/stake/${address}`
-}
